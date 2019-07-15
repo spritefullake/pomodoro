044b0bedc55573d908b55438e8350bf3383184b5
spritefullake <42ahmad@gmail.com>
2019-07-15 11:17:13 -0400

    Basic pomodoro and task skeleton and timer. Plan to add ticking updates soon

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tamatam, a Pomodoro Technique Application
+
+### Philosophy
+Pomodoro is a time and task management technique. 
+
+The purpose of this application is to explore the Rust language and make following Pomodoro simple, ergonomic, and easily-available.
+
+### Process
+
+Usually the steps in Pomodoro are like so:
+
+1. Choose a task
+2. Set the timer for 25 minutes
+3. Complete the task until the timer ends
+4. Verify completion of the pomodoro
+5. Take a break
+6. Continue this generic process for every task 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::thread;
+use std::time;
+
+
+/* a Pomodoro consists of the task 
+needed to be completed in that time*/
+struct Pomodoro {
+    task: Task,
+    duration: time::Duration,
+}
+/// TODO: add channels that can send updates of the timer to the main thread
+/// or try a shared memory solution
+impl Pomodoro {
+    fn start(mut self) -> thread::JoinHandle<()> {
+        println!("Starting pomodoro for {} seconds",self.duration.as_secs());
+        thread::spawn(move || {
+            while self.duration.as_secs() > 0 {
+                thread::sleep(time::Duration::from_secs(1));
+                self.decrement_seconds(1);
+
+            }
+        })
+    }
+    fn decrement_seconds(&mut self, amount : u64) -> &mut Self{
+        self.duration = 
+        time::Duration::from_secs(self.duration.as_secs()  - amount);
+        self
+    }
+}
+
+struct Task{
+    title: String,
+    notes: String,
+    completed: bool,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let task1 = Task{
+        title: String::from("Mowing the lawn"),
+        notes: String::from("Watch out for bugs!"),
+        completed: false
+    };
+
+    let pomo1 = Pomodoro{
+        task: task1,
+        duration: time::Duration::from_secs(5),
+    };
+
+    println!("Let us do this pomodoro!");
+
+    let thread1 = pomo1.start();
+
+    thread1.join().expect("Thread error");
+    
+ 
+    println!("Now your pomodoro has ended!");
+
+
 }
