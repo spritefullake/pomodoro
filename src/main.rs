b7954bc90390dd0dff2eefeeb39ccb610416039c
spritefullake <42ahmad@gmail.com>
2019-07-17 07:01:23 -0400

    experimenting with different implementations of the timer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::sync::mpsc; 
+use std::sync::mpsc::channel; 
 use std::time;
    
 mod cli;
 mod timer;
+
+use std::thread;
 
 /// TODO: Develop an input method for tasks 
 /// and a display method for pomodoros
@@ -10,6 +12,33 @@ mod timer;
 fn main() {
     //The main way to receive updates from pomodoros is through channels
 
-    cli::run(std::env::args());
+    //cli::run(std::env::args());
+
+    let (txTimer, rxTimer) = channel::<timer::Response>();
+    let (txCont, rxCont) = channel::<timer::Request>();
+
+    
+
+    let t = timer::Timer{
+        tx: txTimer,
+        rx: rxCont,
+        duration: time::Duration::from_secs(7)
+    };
+
+    let cont = timer::Controller{
+        tx: txCont,
+        rx: rxTimer,
+        timer: t,
+    };
+
+    cont.watch(t);
+
+    std::thread::sleep(time::Duration::from_secs(2));
+    cont.stop();
+    thread::sleep(time::Duration::from_secs(2));
+    cont.unpause();
+
+    thread::sleep(time::Duration::from_secs(7));
+
 
 }
--- src/timer.rs
+++ src/timer.rs
@@ -1,45 +1,85 @@
+use std::sync::{mpsc, Arc};
 use std::thread;
 use std::time;
-use std::sync::{Arc,mpsc};
 
-/// Manages the timekeeping of a pomodoro. 
+
+/// Manages the timekeeping of a pomodoro.
 pub struct Timer {
-    duration: time::Duration,
-    tx: mpsc::Sender<Response>,
-    rx: mpsc::Receiver<Request>,
-
+    pub duration: time::Duration,
+    pub tx: mpsc::Sender<Response>,
+    pub rx: mpsc::Receiver<Request>,
 }
 
+pub struct Controller {
+    pub tx: mpsc::Sender<Request>,
+    pub rx: mpsc::Receiver<Response>,
+}
+impl Controller {
+
+    pub fn start(&self) {
+        self.tx.send(Request::Start);
+    }
+    pub fn stop(&self){
+        self.tx.send(Request::Pause);
+    }
+
+    pub fn unpause(&self){
+        for received in &self.rx {
+            if let  Response::Pausing(thread) = received {
+                thread.unpark();
+            }
+        };
+    }
+
+    pub fn watch(&self, timer: Timer) {
+        self.start();
+        timer.start();
+    }
+}
 
 /// TODO: add channels that can send updates of the timer to the main thread
 /// or try a shared memory solution
 impl Timer {
-    /// Begins the pomodoro timer with periodic updates sent every second. 
+    /// Begins the pomodoro timer with periodic updates sent every second.
     /// Displaying responsibility falls on the receiver.
     /// There will be one pomodoro timer running at a time, so message passing makes sense.
-    fn start(mut self) -> thread::JoinHandle<Response> {
+    fn start(mut self) -> thread::JoinHandle<Self> {
         thread::spawn(move || {
-            unimplemented!();
+            self.tx.send(Response::Starting);
+            while self.duration.as_secs() > 0 {
+                
+                if let Request::Pause = self.rx.recv_timeout(time::Duration::from_nanos(1000)).unwrap_or_else(|err| Request::Start) {
+                    self.tx.send(Response::Pausing(thread::current()));
+                    thread::park();
+                }
+
+                self.decrement_seconds(1);
+                self.tx.send(Response::Ticking(self.duration));
+                thread::sleep(time::Duration::new(1, 0));
+            }
+
+            self
         })
     }
-    fn decrement_seconds(&mut self, amount : u64) -> &mut Self{
-        self.duration -= time::Duration::new(amount,0);
+
+    fn decrement_seconds(&mut self, amount: u64) -> &mut Self {
+        self.duration -= time::Duration::new(amount, 0);
         self
     }
 }
 
 /// The events emitted during the lifecyle of the timer.
 /// Enums with data contain the remaining duration of the timer
-pub enum Request{
+pub enum Request {
     Start,
     Pause,
     Reset(time::Duration),
     End,
 }
-pub enum Response{
+pub enum Response {
     Starting,
     Ticking(time::Duration),
-    Pausing(time::Duration),
+    Pausing(thread::Thread),
     Resetting,
     Ending,
 }
