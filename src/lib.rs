b364ce0790ac05d92c29f410c4ca1984c375f1d0
spritefullake <42ahmad@gmail.com>
2019-07-27 16:20:16 -0400

    exploring declarative macros with config files

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 #[macro_export]
 macro_rules! map_commands {
-    ($( $command:ident : $value:ident ),+) => {
-        let map = HashMap::new();
+    ($( $command:ident = $type:ty ; $value:ident),+) => {
+        {
+        let mut map = std::collections::HashMap::new();
         $( 
-            map.insert($command, $value);
+            map.insert(stringify!($command), stringify!($value::$type));
+            println!("{}",stringify!($type));
+
         ),+
+
+        
         map
+    }
     };
 }
