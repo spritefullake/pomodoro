7051219919fda6532de6bd35bbe6f063ee1cfc3b
spritefullake <42ahmad@gmail.com>
2019-08-07 13:53:37 -0400

    beginning TDD

--- src/timer_state/event.rs
+++ src/timer_state/event.rs
@@ -1,7 +1,9 @@
-use crate::fsm::Trigger;
-use super::timer::Timer;
+use crate::{
+    fsm::Trigger,
+    timer::Timer,
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Event{
     Start,
     Stop,
@@ -15,7 +17,37 @@ impl Trigger for Event{
             //precondition: the duration is greater than or equal to the
             //amount that will be subtracted from it
             //TODO: finish trigger impl and refactor
+            Event::Tick => {
+                if t.duration.as_secs() >= 1 {
+                    Timer::new(t.duration - std::time::Duration::from_secs(1))
+                }
+                else{
+                    t
+                }
+                
+            }
             _ => t,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn timer_duration_stays_0_when_0(){
+        let data = Timer::new(std::time::Duration::from_secs(0));
+        let data_copy = data.clone();
+        let event = Event::Tick;
+        let new_data = event.trigger(data);
+        assert_eq!(new_data.duration, data_copy.duration);
+    }
+    #[test]
+    fn timer_duration_changes_with_tick(){
+        let data = Timer::new(std::time::Duration::from_secs(1));
+        let data_copy = data.clone();
+        let event = Event::Tick;
+        let new_data = event.trigger(data);
+        assert_ne!(new_data.duration, data_copy.duration);
     }
 }
